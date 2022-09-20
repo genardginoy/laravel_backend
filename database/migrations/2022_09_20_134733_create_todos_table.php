853132140eec5956b2da19cfb5784afd9b589287
deepak <deepamdeebu@gmail.com>
2022-09-20 20:59:45 +0530

    Migration : Add migration for the todos table with basic set of columns

---
+++ database/migrations/2022_09_20_134733_create_todos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('td_id');
+            $table->string('td_title');
+            $table->text('td_description');
+            $table->string('td_status', 1);
+            $table->timestamp('td_created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('td_updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
